67d5b43de04cf585653a03ea6493181efc604226
Joseph Calla <josephestanislao9@gmail.com>
2021-02-21 15:58:14 -0500

    Initial Commit

---
+++ BCPchallenge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BCPchallenge
+//
+//  Created by Joseph Calla Moreyra on 21/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
